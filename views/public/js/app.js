09479145c8eff22619b0defb186fbf43a2930997
Shourya <mittalshourya97@gmail.com>
2017-10-26 20:37:03 +0530

    Making navbar sticky and changing its size on scroll

--- views/public/js/app.js
+++ views/public/js/app.js
@@ -12,6 +12,24 @@ app.config(function($stateProvider,$urlRouterProvider,$locationProvider)
 		})
 })
 
-app.controller('mainpageController',function($scope,$rootScope,$state,$http){
+app.controller('mainpageController',function($scope,$rootScope,$state,$http,$window){
 	console.log("mainpageController called");
+	$scope.navClass="big";
+	$scope.headClass="heading";
+	$scope.linkClass="li";
+	angular.element($window).bind(
+	"scroll", function() {
+         console.log(window.pageYOffset);
+         if(window.pageYOffset >0) {
+           $scope.navClass = 'small';
+           $scope.headClass='heading_on_scroll';
+           $scope.linkClass='li_on_scroll';
+         } else {
+           $scope.navClass = 'big';
+           $scope.headClass="heading";
+           $scope.linkClass="li";
+         }
+         $scope.$apply();
+   });
+	
 })
